2912174e4eb6c445115c76babcf6c26759a70dce
Alexandre Dumont <adumont@gmail.com>
2011-10-21 17:03:13 +0200

    chacha-keypad: HTC_SHARE to FUNC_2

--- liblights/lights.c
+++ liblights/lights.c
@@ -41,6 +41,7 @@ char const*const AMBER_LED_FILE = "/sys/class/leds/amber/brightness";
 char const*const GREEN_LED_FILE = "/sys/class/leds/green/brightness";
 
 char const*const BUTTON_FILE = "/sys/class/leds/button-backlight/brightness";
+char const*const KEYBOARD_FILE = "/sys/class/leds/keyboard-backlight/brightness";
 
 char const*const AMBER_BLINK_FILE = "/sys/class/leds/amber/blink";
 char const*const GREEN_BLINK_FILE = "/sys/class/leds/green/blink";
@@ -209,6 +210,7 @@ static int set_light_buttons (struct light_device_t* dev,
 	int on = is_lit (state);
 	pthread_mutex_lock (&g_lock);
 	err = write_int (BUTTON_FILE, on?255:0);
+        err = write_int (KEYBOARD_FILE, on?255:0);
 	pthread_mutex_unlock (&g_lock);
 
 	return 0;
