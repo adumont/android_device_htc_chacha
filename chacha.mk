060cb197b02a103eb491d66b81be6a7753a778a9
Alexandre Dumont <adumont@gmail.com>
2012-05-20 00:28:58 +0200

    kbd: add other layouts (copies of default for now)

--- chacha.mk
+++ chacha.mk
@@ -39,6 +39,36 @@ DISABLE_DEXPREOPT := false
 PRODUCT_COPY_FILES += \
     device/htc/chacha/key/chacha-keypad.kl:system/usr/keylayout/chacha-keypad.kl \
     device/htc/chacha/key/chacha-keypad.kcm:system/usr/keychars/chacha-keypad.kcm \
+    device/htc/chacha/key/chacha-keypad-ara.kl:system/usr/keylayout/chacha-keypad-ara.kl \
+    device/htc/chacha/key/chacha-keypad-ara.kcm:system/usr/keychars/chacha-keypad-ara.kcm \
+    device/htc/chacha/key/chacha-keypad-aw.kl:system/usr/keylayout/chacha-keypad-aw.kl \
+    device/htc/chacha/key/chacha-keypad-aw.kcm:system/usr/keychars/chacha-keypad-aw.kcm \
+    device/htc/chacha/key/chacha-keypad-bopomo.kl:system/usr/keylayout/chacha-keypad-bopomo.kl \
+    device/htc/chacha/key/chacha-keypad-bopomo.kcm:system/usr/keychars/chacha-keypad-bopomo.kcm \
+    device/htc/chacha/key/chacha-keypad-cn.kl:system/usr/keylayout/chacha-keypad-cn.kl \
+    device/htc/chacha/key/chacha-keypad-cn.kcm:system/usr/keychars/chacha-keypad-cn.kcm \
+    device/htc/chacha/key/chacha-keypad-ell.kl:system/usr/keylayout/chacha-keypad-ell.kl \
+    device/htc/chacha/key/chacha-keypad-ell.kcm:system/usr/keychars/chacha-keypad-ell.kcm \
+    device/htc/chacha/key/chacha-keypad-esn.kl:system/usr/keylayout/chacha-keypad-esn.kl \
+    device/htc/chacha/key/chacha-keypad-esn.kcm:system/usr/keychars/chacha-keypad-esn.kcm \
+    device/htc/chacha/key/chacha-keypad-fra.kl:system/usr/keylayout/chacha-keypad-fra.kl \
+    device/htc/chacha/key/chacha-keypad-fra.kcm:system/usr/keychars/chacha-keypad-fra.kcm \
+    device/htc/chacha/key/chacha-keypad-ger.kl:system/usr/keylayout/chacha-keypad-ger.kl \
+    device/htc/chacha/key/chacha-keypad-ger.kcm:system/usr/keychars/chacha-keypad-ger.kcm \
+    device/htc/chacha/key/chacha-keypad-hk.kl:system/usr/keylayout/chacha-keypad-hk.kl \
+    device/htc/chacha/key/chacha-keypad-hk.kcm:system/usr/keychars/chacha-keypad-hk.kcm \
+    device/htc/chacha/key/chacha-keypad-ita.kl:system/usr/keylayout/chacha-keypad-ita.kl \
+    device/htc/chacha/key/chacha-keypad-ita.kcm:system/usr/keychars/chacha-keypad-ita.kcm \
+    device/htc/chacha/key/chacha-keypad-nor.kl:system/usr/keylayout/chacha-keypad-nor.kl \
+    device/htc/chacha/key/chacha-keypad-nor.kcm:system/usr/keychars/chacha-keypad-nor.kcm \
+    device/htc/chacha/key/chacha-keypad-rus.kl:system/usr/keylayout/chacha-keypad-rus.kl \
+    device/htc/chacha/key/chacha-keypad-rus.kcm:system/usr/keychars/chacha-keypad-rus.kcm \
+    device/htc/chacha/key/chacha-keypad-steng.kl:system/usr/keylayout/chacha-keypad-steng.kl \
+    device/htc/chacha/key/chacha-keypad-steng.kcm:system/usr/keychars/chacha-keypad-steng.kcm \
+    device/htc/chacha/key/chacha-keypad-tai.kl:system/usr/keylayout/chacha-keypad-tai.kl \
+    device/htc/chacha/key/chacha-keypad-tai.kcm:system/usr/keychars/chacha-keypad-tai.kcm \
+    device/htc/chacha/key/chacha-keypad-tur.kl:system/usr/keylayout/chacha-keypad-tur.kl \
+    device/htc/chacha/key/chacha-keypad-tur.kcm:system/usr/keychars/chacha-keypad-tur.kcm \
     device/htc/chacha/key/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
     device/htc/chacha/key/cy8c-touchscreen.kl:system/usr/keylayout/cy8c-touchscreen.kl
 
