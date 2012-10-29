570c149114c59662c1ef8f184a839987266540a4
Alexandre Dumont <adumont@gmail.com>
2012-10-29 17:40:27 +0100

    cm.mk: Define PRODUCT_LOCALES

--- cm.mk
+++ cm.mk
@@ -25,3 +25,16 @@ BUILD_ID := ADLX00$(shell date -u +%Y%m%d)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_chacha BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_chacha/chacha:4.1.1/ADLX$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="2.13.401.2 CL197017 release-keys"
+
+PRODUCT_LOCALES := \
+    en_US \
+    es_ES \
+    ca_ES \
+    en_GB \
+    de_DE \
+    eu_ES \
+    fr_FR \
+    it_IT \
+    pt_PT \
+    pt_BR \
+    mdpi
