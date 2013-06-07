a6c8ce8b3bc90eaf4cd7e18500e03b9342071be4
Alexandre Dumont <adumont@gmail.com>
2013-06-08 00:25:28 +0200

    Change to 4.2.2 in BUILD_FINGERPRINT
    Change-Id: I370529bbe7b2fad5cadbb33608b8ebefb842c48f

--- cm.mk
+++ cm.mk
@@ -27,7 +27,7 @@ else
 endif
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_chacha BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_chacha/chacha:4.1.1/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="2.13.401.2 CL197017 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_chacha BUILD_ID=$(BUILD_ID) BUILD_DISPLAY_ID=$(BUILD_ID) BUILD_FINGERPRINT=cyanogenmod/htc_chacha/chacha:4.2.2/$(BUILD_ID)/0.1:user/release-keys PRIVATE_BUILD_DESC="2.13.401.2 CL197017 release-keys"
 
 PRODUCT_LOCALES := \
     en_US \
